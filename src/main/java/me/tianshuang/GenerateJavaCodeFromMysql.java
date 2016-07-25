5e3644400006aad9150a0c06c34d8bc3aeadbda9
田爽 <tspoison@hotmail.com>
2016-07-25 18:46:20 +0800

    optimize

--- src/main/java/me/tianshuang/GenerateJavaCodeFromMysql.java
+++ src/main/java/me/tianshuang/GenerateJavaCodeFromMysql.java
@@ -43,52 +43,52 @@ public class GenerateJavaCodeFromMysql {
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
 
             Map<String, String> fieldCommentMap = new HashMap<>();
-            ResultSet fieldCommentResultSet = connection.prepareStatement("SHOW FULL COLUMNS FROM " + table).executeQuery();
-            while (fieldCommentResultSet.next()) {
-                fieldCommentMap.put(fieldCommentResultSet.getNString("Field"), fieldCommentResultSet.getNString("Comment"));
+            try (PreparedStatement preparedStatement = connection.prepareStatement("SHOW FULL COLUMNS FROM " + table)) {
+                ResultSet fieldCommentResultSet = preparedStatement.executeQuery();
+                while (fieldCommentResultSet.next()) {
+                    fieldCommentMap.put(fieldCommentResultSet.getNString("Field"), fieldCommentResultSet.getNString("Comment"));
+                }
             }
 
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM " + table + " LIMIT 1");
-            ResultSet resultSet = preparedStatement.executeQuery();
-            ResultSetMetaData metadata = resultSet.getMetaData();
+            try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM " + table + " LIMIT 1")) {
+                ResultSet resultSet = preparedStatement.executeQuery();
+                ResultSetMetaData metadata = resultSet.getMetaData();
+                TypeSpec.Builder builder = TypeSpec.classBuilder(lowerUnderscoreToUpperCamel(table)).addAnnotation(Data.class)
+                        .addModifiers(Modifier.PUBLIC);
+                for (int i = 1; i <= metadata.getColumnCount(); i++) {
+                    String columnClass = metadata.getColumnClassName(i);
+                    Class clazz = null;
+                    switch (columnClass) {
+                        case "java.math.BigInteger":
+                        case "java.lang.Long":
+                            clazz = Long.class;
+                            break;
+                        case "java.lang.Integer":
+                            clazz = Integer.class;
+                            break;
+                        case "java.lang.String":
+                            clazz = String.class;
+                            break;
+                        case "java.sql.Timestamp":
+                            clazz = LocalDateTime.class;
+                            break;
+                        default:
+                            System.out.println(columnClass);
+                    }
+                    String fieldName = lowerUnderscoreToLowerCamel(metadata.getColumnName(i));
+                    FieldSpec.Builder fieldSpecBuilder = FieldSpec.builder(clazz, fieldName)
+                            .addModifiers(Modifier.PRIVATE);
+                    String fieldComment = fieldCommentMap.get(metadata.getColumnName(i));
+                    if (!StringUtils.isNullOrEmpty(fieldComment)) {
+                        fieldSpecBuilder.addJavadoc(fieldComment + "\n");
+                    }
+                    builder.addField(fieldSpecBuilder.build());
+                }
 
-            TypeSpec.Builder builder = TypeSpec.classBuilder(lowerUnderscoreToUpperCamel(table)).addAnnotation(Data.class)
-                    .addModifiers(Modifier.PUBLIC);
-            for (int i = 1; i <= metadata.getColumnCount(); i++) {
-                String columnClass = metadata.getColumnClassName(i);
-                Class clazz = null;
-                switch (columnClass) {
-                    case "java.math.BigInteger":
-                    case "java.lang.Long":
-                        clazz = Long.class;
-                        break;
-                    case "java.lang.Integer":
-                        clazz = Integer.class;
-                        break;
-                    case "java.lang.String":
-                        clazz = String.class;
-                        break;
-                    case "java.sql.Timestamp":
-                        clazz = LocalDateTime.class;
-                        break;
-                    default:
-                        System.out.println(columnClass);
-                }
-                String fieldName = lowerUnderscoreToLowerCamel(metadata.getColumnName(i));
-                FieldSpec.Builder fieldSpecBuilder = FieldSpec.builder(clazz, fieldName)
-                        .addModifiers(Modifier.PRIVATE);
-                String fieldComment = fieldCommentMap.get(metadata.getColumnName(i));
-                if (!StringUtils.isNullOrEmpty(fieldComment)) {
-                    fieldSpecBuilder.addJavadoc(fieldComment + "\n");
-                }
-                builder.addField(fieldSpecBuilder.build());
+                JavaFile javaFile = JavaFile.builder(packageName, builder.build())
+                        .build();
+                javaFile.writeTo(Paths.get("."));
             }
-
-            JavaFile javaFile = JavaFile.builder(packageName, builder.build())
-                    .build();
-
-            javaFile.writeTo(Paths.get("."));
-
         } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
